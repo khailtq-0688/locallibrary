eb9c0ce50511a9b3e65ac1f6857e05c8a0800a4a
KhaiLeeTran <khailtq.2004@gmail.com>
2025-07-23 15:30:48 +0700

    Part11 Django web application security

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from django.urls import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('secureadmin/', admin.site.urls),
 ]
 
 urlpatterns += [
